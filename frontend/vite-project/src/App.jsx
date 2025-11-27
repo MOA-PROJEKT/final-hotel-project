6de4bcb7285c6a20f1ce37d02a160dc484dec4a6
Misha <mikamishka@yahoo.de>
2025-11-27 12:21:17 +0100

    Backend Grundstruktur

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,14 +1,9 @@
-
-import './App.css'
-
-function App() {
- 
-
+export default function App() {
   return (
-    <>
-      
-    </>
-  )
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-semibold text-emerald-300">
+        MOA Haven – Tailwind läuft 🎉
+      </h1>
+    </div>
+  );
 }
-
-export default App
