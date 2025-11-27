5dc0bf3c8cb86a29243a36df2147c499346e86f2
Misha <mikamishka@yahoo.de>
2025-11-27 13:35:28 +0100

    Footer Legen

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,9 +1,15 @@
+import Footer from "./components/layout/Footer";
+
 export default function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-semibold text-emerald-300">
-        MOA Haven – Tailwind läuft 🎉
-      </h1>
+    <div className="min-h-screen flex flex-col">
+      <main className="flex-1 flex items-center justify-center">
+        <h1 className="text-4xl font-semibold text-emerald-500">
+          MOA Haven – Tailwind läuft 🎉
+        </h1>
+      </main>
+
+      <Footer />
     </div>
   );
 }
